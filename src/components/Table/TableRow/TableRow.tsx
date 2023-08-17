c7abc8e16f8ba58de36247f700d326496ef4c76c
Kamil Radziszewski <contact@kamilradziszewski.com>
2023-08-17 23:54:30 +0200

    Add IP error message display

--- src/components/Table/TableRow/TableRow.tsx
+++ src/components/Table/TableRow/TableRow.tsx
@@ -1,3 +1,5 @@
+import { LuAlertCircle } from "react-icons/lu";
+
 import { IPRecord } from "../../../stores/dataStore";
 import { useStore } from "../../../stores/store";
 import { formatDate } from "../../../utils/formatDate";
@@ -8,13 +10,31 @@ function TableRow({ record }: { record: IPRecord }) {
   const requestTimeArray = formatDate(record.requestTime);
   const responseTimeArray = formatDate(record.responseTime);
 
+  const hasStatusFail = record.status === "fail";
+
   return (
     <tr
       key={record.query}
-      className="Table__TbodyTr"
-      onClick={() => modalStore.setModalData(record)}
+      className={`Table__TbodyTr ${
+        record.status === "fail" ? "Table__TbodyTr--Error" : ""
+      }`}
+      onClick={() => (hasStatusFail ? null : modalStore.setModalData(record))}
     >
-      <td className="Table__Td">{record.query}</td>
+      <td className="Table__Td">
+        <div
+          className={`Table__TdContent ${
+            hasStatusFail ? "Table__TdContent--Error" : ""
+          }`}
+        >
+          {record.query}
+          {hasStatusFail && (
+            <div className="Table__Error">
+              <LuAlertCircle className="Table__ErrorIcon" />
+              <div className="Table__ErrorTooltip">Error: {record.message}</div>
+            </div>
+          )}
+        </div>
+      </td>
       <td className="Table__Td">
         {requestTimeArray[0]}
         <span className="Timestamp__Milliseconds">.{requestTimeArray[1]}</span>
